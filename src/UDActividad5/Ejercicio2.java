aecbc2d54091ca9497772237427ef499a93a7e35
sergiorm21 <reina.moser21@cordoba.salesianos.edu>
2022-10-14 18:14:59 +0200

    actualizacionejercicio2

--- src/UDActividad5/Ejercicio2.java
+++ src/UDActividad5/Ejercicio2.java
@@ -3,16 +3,27 @@ package UDActividad5;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Ejercicio2 {
 
     public static void main(String[] args) {
+
+        FileWriter archivo = null;
+        PrintWriter pw = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             String ruta;
             File archivo = null;
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+
+            FileWriter archivo = null;
+            PrintWriter pw = null;
 
             System.out.println("Bienvenido, selecciona lo que quieres hacer.");
 
@@ -27,15 +38,53 @@ public class Ejercicio2 {
                 case 1:
                     ruta = "C:\\Users\\reina.moser\\Documents\\NetBeansProjects\\AccesoADatos\\src\\UDActividad5\\SergioReinaExtra1.txt";
                     archivo = new File(ruta);
-                    
-                    if (!archivo.exists()){
+
+                    if (!archivo.exists()) {
                         archivo.createNewFile();
-                    }else{
+                    } else {
                         System.out.println("No se puede crear porque ya existe.");
                     }
-                    
+
                 case 2:
-                    
+                    try {
+                    ruta = "C:\\Users\\reina.moser\\Documents\\NetBeansProjects\\AccesoADatos\\src\\UDActividad5\\SergioReinaExtra1.txt";
+                    archivo = new File(ruta);
+
+                    if (!archivo.exists()) {
+                        System.out.println("El archivo ya existe");
+                    } else {
+                        FileReader fr = new FileReader(archivo);
+                        br = new BufferedReader(fr);
+
+                        String linea = br.readLine();
+
+                        while (linea != null) {
+                            System.out.println(linea);
+                            linea = br.readLine();
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                case 3:
+                    try {
+                    ruta = "C:\\Users\\reina.moser\\Documents\\NetBeansProjects\\AccesoADatos\\src\\UDActividad5\\SergioReinaExtra1.txt";
+                    archivo = new FileWriter(ruta);
+                    Printwriter pw = new Printwriter(archivo);
+
+                    if (!archivo.exists()) {
+                        System.out.println("El archivo ya existe.");
+                    } else {
+                        for (int i = 0; i < 10 , i++) {
+                            pw.println("Linea " + 1);
+                        }
+                    }
+
+                } catch (Excepcion e) {
+                    e.printStackTrace();
+                }
 
             }
         } catch (Exception e) {
---
+++ src/UDActividad5/pw.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UDActividad5;
+
+/**
+ *
+ * @author reina.moser
+ */
+class pw {
+
+    static void println(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
